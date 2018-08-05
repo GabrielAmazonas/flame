3f137532cd0b339068268ebb0702e2fbcd2f4a65
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2018-08-05 12:58:30 -0300

    Adding creation date to models

--- models/request_log.py
+++ models/request_log.py
@@ -1,9 +1,10 @@
 import datetime
-from mongoengine import Document, StringField, QuerySetManager
+from mongoengine import Document, StringField, DateTimeField, QuerySetManager
 
 
 class RequestLog(Document):
     user = StringField(required=True)
     request_body = StringField(required=True)
     request_headers = StringField(required=True)
+    date = DateTimeField(default=datetime.datetime.utcnow)
     objects = QuerySetManager()
--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 import datetime
-from mongoengine import Document, StringField, QuerySetManager
+from mongoengine import Document, StringField, DateTimeField, QuerySetManager
 
 
 class User(Document):
     email = StringField(required=True)
     password = StringField(max_length=200, required=True)
+    creation_date = DateTimeField(default=datetime.datetime.utcnow)
     objects = QuerySetManager()
