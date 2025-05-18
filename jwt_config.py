b2214bf9c6bc9304582cf333b12fadba6d19fb76
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2025-05-18 11:22:02 -0300

    Updating docker container strategy

--- jwt_config.py
+++ jwt_config.py
@@ -1,5 +1,12 @@
 import os
 
-JwtConfig = {
-    'key' : os.environ.get('JWT_KEY', 'mysecretkey')
+"""
+JWT configuration for Flask app.
+
+Environment variable required:
+ - JWT_KEY: The secret signing key for JWT tokens. (Never check this into version control.)
+"""
+
+JWT_CONFIG = {
+    'key': os.environ.get('JWT_KEY', 'mysecretkey')
 }
